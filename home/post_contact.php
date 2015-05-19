02c6e4d739ea7c0a88ece3f329f7cc9503465adb
Pablo Faust <pabfaust@gmail.com>
2015-05-19 12:02:18 -0500

    Test mailgun

--- home/post_contact.php
+++ home/post_contact.php
@@ -10,13 +10,13 @@ use Mailgun\Mailgun;
 $mg = new Mailgun("key-5d424be0c125221134ef28fdd39d66b0");
 $domain = "app42401434151a4b48bb4312c467ada548.mailgun.org";
 
-$mg->sendMessage($doamin, array(
-'from' => $_POST["mail"],
+$mg->sendMessage($domain, array(
+'from' => "pabfaust@gmail.com" //$_POST["mail"],
 'to' => 'postmaster@app42401434151a4b48bb4312c467ada548.mailgun.org',
 'subject' => 'TEST',
-'text' => $_POST["message"]
+'text' => "hola" //$_POST["message"]
 	)
-)
+);
  
 // echo "<script>alert('Merci, votre message nous a bien été envoyé ! On vous répondra au plus vite !')</script>";
 // echo "<SCRIPT>history.back(1)</SCRIPT>"; 
